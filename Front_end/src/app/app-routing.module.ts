cfbde888d2ab2a03fee663653de5417d9caa81fa
Elizabeth Chialva <chialvaelizabeth@gmail.com>
2023-05-03 16:41:29 -0300

    Agrega texto a quienes somos

--- Front_end/src/app/app-routing.module.ts
+++ Front_end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuienesSomosComponent } from "./pages/quienes-somos/QuienesSomosComponent";
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:'quienes_somos', component: QuienesSomosComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
