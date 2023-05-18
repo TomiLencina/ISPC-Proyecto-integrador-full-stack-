a0f38a1ece79551506edef517b95a63d210ff77d
ahroncich7 <alehroncich@gmail.com>
2023-05-18 14:10:29 -0300

    FEAT: Declara models iniciales

--- Back_end/controlling/controlling_app/models.py
+++ Back_end/controlling/controlling_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=20)
+    alias = models.CharField(max_length=20)
+    nationality = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"User: {self.alias} from {self.nationality}, email: {self.email}"
+
+
+class Portfolio(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    isPremium = models.BooleanField(default=False)
+
+
+class Asset(models.Model):
+    simbol = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    currency= models.CharField(max_length=4)
+    broker= models.CharField(max_length=20) 
+
+
+class Operation(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    buy_date = models.DateField()
+    buy_price = models.IntegerField()
+    quantity = models.IntegerField(default=1)
+    last_price = models.IntegerField(default=buy_price)
+    sell_date = models.DateField(null=True)
