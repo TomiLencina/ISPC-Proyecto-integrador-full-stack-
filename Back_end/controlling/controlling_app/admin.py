a076639dedb9d8d120c5b9bfaf3dd96332a6a0c3
NicolasLucero <nicolucerocipo10@gmail.com>
2023-05-19 15:43:28 -0300

    CRUD

--- Back_end/controlling/controlling_app/admin.py
+++ Back_end/controlling/controlling_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Portfolio, Asset, Operation
 
 # Register your models here.
+
+
+
+admin.site.register(User)
+admin.site.register(Portfolio)
+admin.site.register(Asset)
+admin.site.register(Operation)
