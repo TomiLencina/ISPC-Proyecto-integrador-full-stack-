a4cde29f1425acd328c9e7912e7f9880a07237c0
NicolasLucero <nicolucerocipo10@gmail.com>
2023-06-04 22:56:06 -0300

    Servicio registro

--- Front_end/src/app/pages/registro/registro.component.ts
+++ Front_end/src/app/pages/registro/registro.component.ts
@@ -3,72 +3,76 @@ import { UserService } from 'src/app/services/user.service';
 import { Form, FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
+
+
+
+
 @Component({
-    selector: 'app-registro',
-    templateUrl: './registro.component.html',
-    styleUrls: ['./registro.component.css'],
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
+  confirmPass!: string;
+  registryForm: FormGroup;
 
-    confirmPass!: string
-    registryForm: FormGroup
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+    this.registryForm = this.formBuilder.group({
+      password: ["", [Validators.required, Validators.minLength(4)]],
+      confirmPassword: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      nationality: ["", [Validators.required]],
+      username: ["", [Validators.required]],
+    });
+  }
 
-    constructor(private formBuilder: FormBuilder) {
-    
-        this.registryForm = this.formBuilder.group(
-            {
-                password: ["", [Validators.required, Validators.minLength(4)]],
-                confirmPassword: ["", [Validators.required]],
-                email: ["", [Validators.required, Validators.email]],
-                nationality: ["", [Validators.required]],
-                username: ["", [Validators.required]],
-            }
-        )
-
-
+  compare() {
+    if (this.registryForm.controls['password'].value != this.confirmPass) {
+      this.registryForm.controls['confirmPassword'].setErrors({ 'incorrect': true });
     }
+  }
 
-    compare() {
-        
-        if (this.registryForm.controls['password'].value != this.confirmPass) {
+  get Email() {
+    return this.registryForm.get("email");
+  }
 
-            this.registryForm.controls['confirmPassword'].setErrors({ 'incorrect': true });
-        }
+  get Password() {
+    return this.registryForm.get("password");
+  }
 
+  get ConfirmPassword() {
+    return this.registryForm.get("confirmPassword");
+  }
+
+  get Nationality() {
+    return this.registryForm.get("nationality");
+  }
+
+  get Username() {
+    return this.registryForm.get("username");
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.registryForm.valid) {
+      const formData = {
+        username: this.registryForm.value.username,
+        nationality: this.registryForm.value.nationality,
+        email: this.registryForm.value.email,
+        password: this.registryForm.value.password
+      };
+
+      this.userService.sendFormData(formData)
+        .subscribe(
+          (response: any) => {
+            console.log(response);
+          },
+          (error: any) => {
+          }
+        );
+    } else {
+
+      this.registryForm.markAllAsTouched();
     }
-
-    get Email() {
-        return this.registryForm.get("email")
-    }
-
-    get Password() {
-        return this.registryForm.get("password")
-    }
-
-    get ConfirmPassword() {
-        return this.registryForm.get("confirmPassword")
-    }
-
-    get Nationality() {
-        return this.registryForm.get("nationality")
-    }
-
-    get Username() {
-        return this.registryForm.get("username")
-    }
-
-    onSubmit(event: Event){
-        event.preventDefault();
-        if (this.registryForm.valid){
-        
-            //Make the request with formdata
-            
-        }else{
-
-            //Validate fields 
-            this.registryForm.markAllAsTouched()
-        }
-        
-    }
-
+  }
 }
