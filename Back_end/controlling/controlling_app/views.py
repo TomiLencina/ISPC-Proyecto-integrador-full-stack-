298a1d4fe3cfea4cc8e717cf591c5ab7dc091302
ahroncich7 <alehroncich@gmail.com>
2023-06-19 18:55:19 -0300

    FEAT: Permite creacion de una operación.

--- Back_end/controlling/controlling_app/views.py
+++ Back_end/controlling/controlling_app/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.shortcuts import HttpResponse
 from django.db import transaction
-from .serializer import UserSerializer
+from .serializer import UserSerializer, OperationSerializer, PortfolioSerializer
+from .models import Portfolio
 
 
 
@@ -15,6 +16,25 @@ def index(request):
 @permission_classes([IsAuthenticated])
 def protected(request):
     return Response({"content": "Esta vista está protegida"})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_portfolio(request):
+    user = request.user
+    portfolio = user.portfolio
+    operations = portfolio.operations.all()
+    serializer = OperationSerializer(operations, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_operation(request):
+    serializer = OperationSerializer(data=request.data)
+    if serializer.is_valid():
+        portfolio = request.user.portfolio
+        operation = serializer.save(portfolio=portfolio)
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 
 @api_view(['POST'])
@@ -28,5 +48,7 @@ def register_user(request):
         user = serializer.save()
         user.set_password(request.data.get('password'))
         user.save()
+        portfolio = Portfolio(user=user)
+        portfolio.save()
 
     return Response({"content": "Usuario creado exitosamente."}, status=200)
